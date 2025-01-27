508b4316dbd875b2069f7103f9cdfae441331095
Noam1303 <159448369+Noam1303@users.noreply.github.com>
2025-01-27 10:47:15 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -33,6 +33,9 @@ CKEDITOR.editorConfig = function( config ) {
 	// Set the most common block elements.
 	config.format_tags = 'p;h1;h2;h3;pre';
 
+	//replace <p> tag with <br> tag
+	config.enterMode = CKEDITOR.ENTER_BR;
+
 	// Simplify the dialog windows.
 	config.removeDialogTabs = 'image:advanced;link:advanced';
 };
